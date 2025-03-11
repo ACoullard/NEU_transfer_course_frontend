5877f2e7377ebeeb7df3db2da4bf3f11281f9f85
acoullard <andrecoullard@gmail.com>
2025-03-11 01:35:58 -0400

    Add table, and logic for filling out table as well as fetching updates from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,77 @@
 import { useEffect, useState, useRef, ReactElement } from 'react'
 import './App.css'
-
+const SERVER_URL = 'http://127.0.0.1:5000/api'
 let firstRun = true
 type ClassList = {[key:string]: string[]}
 
-function fetchClassList() {
+async function fetchClassList() {
   // fetches the list of available NEU classes in dictionary form
   // classlist[department] = [code1, code2, ...]
-  return {"ENG":["101"], "MATH":["1365", "2000", "101"]}
+  const url = SERVER_URL.concat('/courseslist')
+  try {
+    const responce = await fetch(url, {
+      method: 'GET',
+      mode: 'cors'
+    })
+    console.log("responce", responce)
+    const json = await responce.json()
+    console.log("json", json, json[0])
+    return json
+  }
+  catch {
+    console.error("AHHHHH!!!!! ERROR!!")
+  }
+
+  // return {"CS":["3000", "3500"], "MATH":["1365"]}
 }
 
+async function fetchCourses(courseCode: string) {
+  const url = SERVER_URL.concat('/course?class_code=', courseCode)
+  try {
+    const responce = await fetch(url, {
+      method: 'GET',
+      mode: 'cors'
+    })
+    console.log("responce", responce)
+    const json = await responce.json()
+    console.log("json", json, json[0])
+    return json
+  }
+  catch {
+    console.error("AHHHHH!!!!! ERROR!!")
+  }
+  
+  // return [["MATH 281","Alfred University"],["MATH 130","American College of Thessaloniki, The"]]
+}
 
 function buildOptions(optionsValues: string[]) {
   const deptOptions = []
   for(let i = 0; i < optionsValues.length; i++) {
     deptOptions.push(<option key={i}>{optionsValues[i]}</option>)
   }
-  console.log(deptOptions)
   return deptOptions
+}
+
+function buildTable(courses: string[][]) {
+  const tableRows = []
+  let count = 0
+  for (const coursePair of courses) {
+    const row = <tr key={count}>
+      <th className="p-4">
+        <p className="text-sm leading-none font-normal">
+          {coursePair[0]}
+        </p>
+      </th>
+      <th className="p-4">
+        <p className="text-sm leading-none font-normal">
+          {coursePair[1]}
+        </p>
+      </th>
+    </tr>
+    tableRows.push(row)
+    count++
+  }
+  return tableRows
 }
 
 function App() {
@@ -27,50 +81,81 @@ function App() {
   const classList = useRef<ClassList | null>(null)
   const [deptOptions, setDeptOptions] = useState<ReactElement[] | null>(null)
   let codeOptions = null
+  const [tableRows, setTableRows] = useState<ReactElement[] | null>(null)
+
+  function displayTable(code: string) {
+    const rawCourses = fetchCourses(code)
+    rawCourses.then((res) => {setTableRows(buildTable(res))})
+  }
 
   // this only runs on the first render
   useEffect(() => {
     if (firstRun) {
       firstRun = false
-      
-      classList.current = fetchClassList()
-      const departments = Object.keys(classList.current)
-
-      setDeptOptions(buildOptions([''].concat(departments)))
+      fetchClassList().then((res) => {
+        classList.current = res
+        setDeptOptions(buildOptions([''].concat(Object.keys(res))))
+      })
+      // const departments = Object.keys(classList.current)
+      // setDeptOptions(buildOptions([''].concat(departments)))
     }
   }, [])
 
-  if (dept == '' || classList.current == null){
-    codeOptions = [<option key='0'></option>]
+
+  if (!dept || classList.current == null) {
+    codeOptions = [<option key="0"></option>];
   } else {
-    codeOptions = buildOptions(classList.current[dept])
+    codeOptions = buildOptions(classList.current[dept]);
   }
 
   return (
     <>
     {/* Main div. Just centers*/}
-    <div className='h-screen flex justify-center items-center'>
-      <div className='flex flex-col gap-4'>
+    <div className='h-screen flex justify-center items-center flex-col'>
+      <div className='flex flex-col gap-4 mt-30'>
         <h1 className='text-3x1' >Hello, this is the NEU Reverse transfer class finder</h1>
+        <div className='flex flex-col sm:flex-row'>
+          <select 
+            value={dept}
+            onChange={(e) => {
+              const newDept = e.target.value
+              setDept(newDept)
+              if (newDept && classList.current){
+                const newCode = classList.current[newDept][0]
+                setCode(newDept.concat(newCode))
+                displayTable(newDept.concat(newCode))
+              } else {
+                setCode('')
+              }
+            }}>
+            {deptOptions}
+          </select>
 
-        <select 
-          value={dept}
-          
-          onChange={(e) => {
-          setDept(e.target.value)
-        }}>
-          {deptOptions}
-        </select>
-
-        <select 
-          value={code}
-          
-          onChange={(e) => {
-          setCode(e.target.value)
-        }}>
-          {codeOptions}
-        </select>
-        <p className='mt-4'>{dept} {code}</p>
+          <select 
+            value={code}
+            onChange={(e) => {
+              const newCode = e.target.value
+              console.log(newCode)
+              setCode(newCode)
+              displayTable(dept.concat(newCode))
+          }}>
+            {codeOptions}
+          </select>
+        </div>
+      </div>
+          {/* <p className='mt-4'>{dept} {code}</p> */}
+      <div className='overflow-y-auto'>
+        <table className="w-full table-auto min-w-max text-slate-800 mt-16">
+          <thead>
+            <tr className="text-slate-500 border-b border-slate-300 bg-slate-50">
+              <th>Course</th>
+              <th>Institution</th>
+            </tr>
+          </thead>
+          <tbody className='table-auto'>
+            {tableRows}
+          </tbody>
+        </table>
       </div>
     </div>
     </>
